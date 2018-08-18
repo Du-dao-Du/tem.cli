41aad025008dff22d2d7e9ac2ac7cfcaa3b6249e
wudiqing <wudiqing@bigo.sg>
2018-08-18 17:42:34 +0800

    修改node目录

--- temp/node/index.js
+++ temp/node/index.js
@@ -1,9 +1,25 @@
 const Creator = require('../create');
+const inquirer = require('inquirer');
 
+const projectType = {
+  '默认': 'local',
+  'web项目': 'web'
+}
 class NodeCreator extends Creator {
   constructor() {
     super(__dirname);
   }
+  async create(projectPath) {
+    const result = await inquirer.prompt({
+      type: 'list',
+      name: 'answer',
+      message: '请选择要创建的项目类型:',
+      choices: Object.keys(projectType)
+    });
+    // console.log(projectType[result.answer]);
+    this.copy(projectPath, `${this.root}/temp/${projectType[result.answer]}`);
+  }
 }
 
 module.exports = NodeCreator;
+
